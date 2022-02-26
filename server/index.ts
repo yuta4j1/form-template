ea5f7ebbbd0151a707486809ca49a0d756a4738a
yuta4j1 <kascado.ys10@gmail.com>
2022-02-26 13:16:04 +0900

    サーバ追加(multipart/form-data の検証)

---
+++ server/index.ts
@@ -0,0 +1,54 @@
+import Fastify from "fastify"
+import cors from "fastify-cors"
+import util from "util"
+import fs from "fs"
+const { pipeline } = require("stream")
+const pump = util.promisify(pipeline)
+
+const fastify = Fastify({
+  logger: true,
+})
+
+fastify.register(cors)
+fastify.register(require("fastify-multipart"))
+
+fastify.post("/file_upload", async function (req, reply) {
+  // process a single file
+  // also, consider that if you allow to upload multiple files
+  // you must consume all files otherwise the promise will never fulfill
+//   console.log("req", req)
+  const data = await req.file()
+  console.log("file", data)
+
+  data.file // stream
+  data.fields // other parsed parts
+  data.fieldname
+  data.filename
+  data.encoding
+  data.mimetype
+
+  // to accumulate the file in memory! Be careful!
+  //
+  // await data.toBuffer() // Buffer
+  //
+  // or
+
+  await pump(data.file, fs.createWriteStream(data.filename))
+
+  console.log(data.file)
+
+  // be careful of permission issues on disk and not overwrite
+  // sensitive files that could cause security risks
+
+  // also, consider that if the file stream is not consumed, the promise will never fulfill
+
+  reply.send()
+})
+
+fastify.listen(1234, "0.0.0.0", function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`server listening on ${address}`)
+})
