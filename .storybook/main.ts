2b4a89c474b922152ddad4ee441f6d59eec23b36
yuta4j1 <kascado.ys10@gmail.com>
2022-03-05 11:35:29 +0900

    storybook version up

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,10 @@
-const path = require("path")
+const path = require("path");
 
-const toPath = _path => path.join(process.cwd(), _path)
+const toPath = _path => path.join(process.cwd(), _path);
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/react",
   typescript: {
     check: false,
@@ -16,21 +12,20 @@ module.exports = {
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: prop =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-    },
-  },
-  webpackFinal: async config => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": toPath("node_modules/@emotion/react"),
-          "emotion-theming": toPath("node_modules/@emotion/react"),
-        },
-      },
+      propFilter: prop => prop.parent ? !/node_modules/.test(prop.parent.fileName) : true
     }
   },
-}
+  webpackFinal: async config => {
+    return { ...config,
+      resolve: { ...config.resolve,
+        alias: { ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react")
+        }
+      }
+    };
+  },
+  core: {
+    builder: "webpack5"
+  }
+};
