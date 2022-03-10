dd72e9f321868f253d12ce78bca92a500c227a61
yuta4j1 <kascado.ys10@gmail.com>
2022-03-10 20:47:44 +0900

    修正

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,23 @@ import Fastify from "fastify"
 import cors from "fastify-cors"
 import util from "util"
 import fs from "fs"
-import path from 'path'
+import path from "path"
 const { pipeline } = require("stream")
 const pump = util.promisify(pipeline)
+import cookie, { FastifyCookieOptions } from "fastify-cookie"
+import bearerAuthPlugin, { FastifyBearerAuthOptions } from "fastify-bearer-auth"
+import fastifyStatic from "fastify-static"
+import { initializeApp } from "firebase-admin/app"
+import admin from "firebase-admin"
+import { getAuth } from "firebase-admin/auth"
+import firebaseConfig from "./firebaseConfig"
+import sakJson from "./serviceAccountKey.json"
+import pointOfViewPlugin from "point-of-view"
+import ejs from "ejs"
+
+const app = initializeApp({
+  credential: admin.credential.cert(sakJson),
+})
 
 const fastify = Fastify({
   logger: true,
@@ -12,20 +26,62 @@ const fastify = Fastify({
 
 fastify.register(cors)
 fastify.register(require("fastify-multipart"))
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'assets'),
-  // prefix: '/' // optional: default '/'
+fastify.register(pointOfViewPlugin, {
+  engine: {
+    ejs: ejs,
+  },
+})
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "assets"),
+  prefix: "/assets/", // optional: default '/'
+})
+// fastify.register(bearerAuthPlugin, {
+//   auth: (key, req) => {},
+// } as FastifyBearerAuthOptions)
+fastify.register(cookie, {
+  secret: "__sessionid", // for cookies signature
+  parseOptions: {}, // options for parsing cookies
+} as FastifyCookieOptions)
+
+fastify.get("/auth", async (request, reply) => {
+  const bearerToken = request.headers.authorization
+  const idToken = bearerToken.split("Bearer ")[1]
+  console.log(idToken)
+  // Set session expiration to 24Hr.
+  const expiresIn = 60 * 60 * 24 * 1000
+  try {
+    const _ = await getAuth().verifyIdToken(idToken)
+    const sessionCookie = await getAuth().createSessionCookie(idToken, {
+      expiresIn,
+    })
+    return reply
+      .setCookie("__sessionid", sessionCookie, {
+        maxAge: expiresIn,
+        httpOnly: true,
+      })
+      .send({ success: true })
+  } catch (err) {
+    reply.status(401).send({ success: false })
+  }
 })
 
-fastify.get('*', async (request, reply) => {
-  return reply.sendFile('index.html')
+fastify.get("*", async (request, reply) => {
+  console.log(process.env.FUGA)
+  console.log(process.env.HOGE)
+  // const cs = request.cookies
+  // if (cs && cs.__sessionid) {
+  //   return reply.sendFile("index.html")
+  // } else {
+  //   return reply.view("dist/assets/login.ejs", { fuga: process.env.GOGO })
+  // }
+  return reply.sendFile("index.html")
 })
 
 fastify.post("/file_upload", async function (req, reply) {
   // process a single file
   // also, consider that if you allow to upload multiple files
   // you must consume all files otherwise the promise will never fulfill
-//   console.log("req", req)
+  //   console.log("req", req)
   const data = await req.file()
   console.log("file", data)
 
